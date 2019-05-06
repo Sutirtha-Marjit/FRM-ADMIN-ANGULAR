45b4da7300b5c0aae73478a288fc05c151080130
Sutirtha Lenovo Laptop <sutirtha.social.marjit@gmail.com>
2019-05-06 22:34:34 +0530

    Added base URL

--- WingSureAdmin/src/app/facility/request-url.service.ts
+++ WingSureAdmin/src/app/facility/request-url.service.ts
@@ -5,5 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class RequestURLService {
 
+  private baseURL = 'http://ec2-18-236-104-250.us-west-2.compute.amazonaws.com:8080/';
   constructor() { }
+
+  private urlBOX = {
+    fileUpload:'wingsure_crop_insurance-0.0.1/rest/api/uploadfile';
+  };
+  getBaseURL(){
+    return this.baseURL;
+  }
+  getAPIURLS(){
+    return this.urlBOX;
+  }
 }
